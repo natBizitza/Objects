e4c0051b333820755f1148f5cc875de5d06b4224
Natalia Romankevich <40891986+natBizitza@users.noreply.github.com>
2018-10-15 13:23:34 +0200

    Final

---
+++ Objects1/Objects1/Finanzas .cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objects1
+{
+    class Finanzas
+    {
+        private double dinero;
+        private double exchangeRate;
+
+
+        //constructors
+
+        public Finanzas()
+        {
+            this.exchangeRate = 0.92;
+        }
+
+        public Finanzas(double exchangeRate)
+        {
+            this.exchangeRate = exchangeRate;
+        }
+        //métodos get y set
+        //public double GetDinero()
+        //{
+        //    return dinero;
+        //}
+        //public void SetDinero(double dinero)
+        //{
+        //    this.dinero = dinero;
+        //}
+
+        //methods eurosToDolares
+        public double EurosToDollars(double cantidad)
+        {
+           return cantidad* exchangeRate;
+        }
+        public double DollarsToEuros(double cantidad)
+        {
+            return cantidad / exchangeRate;
+        }
+    }
+}
---
+++ Objects1/Objects1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objects1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Finanzas f1 = new Finanzas();
+            Console.WriteLine("10 Euros son " + f1.EurosToDollars(10) + " dolares");
+            Console.WriteLine("10 Dolares son " + f1.DollarsToEuros(10) + " euros");
+            Console.ReadLine();
+        }
+    }
+}
