8ffbf868047f5825f7567d2627ad86a79d2e62bd
Natalia Romankevich <40891986+natBizitza@users.noreply.github.com>
2018-10-15 17:20:18 +0200

    final version

---
+++ ObjectsMarinas/ObjectsMarinas/Marinas.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectsMarinas
+{
+    class Marinas
+    {
+        private double marinas;
+
+        public Marinas()
+        {
+            
+        }
+
+        public double MillasAMetros(double marinas)
+        {
+           return marinas * 1852;
+        }
+
+        public double MillasAKilometros(double marinas)
+        {
+            return marinas * 1.852;
+        }
+    }
+}
---
+++ ObjectsMarinas/ObjectsMarinas/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectsMarinas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Marinas m1 = new Marinas();
+
+            Console.WriteLine("100 nautical miles is " + m1.MillasAMetros(100) + " meters and " + m1.MillasAKilometros(100) + " km.");
+            Console.ReadLine();
+        }
+    }
+}
