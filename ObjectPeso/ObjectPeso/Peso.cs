0f24924310df399832ed5195e6d52b0eec3a9410
Natalia Romankevich <40891986+natBizitza@users.noreply.github.com>
2018-10-15 16:54:33 +0200

    update

---
+++ ObjectPeso/ObjectPeso/Peso.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectPeso
+{
+    class Peso
+    {
+        private double pesoKg;
+        private string measure;
+        //Li, Oz, P, K, G, Q;
+
+        //constructors
+        public Peso()
+        {
+
+        }
+        public Peso(double pesoKg, string measure)
+        {
+
+        }
+
+        public double GetPounds(double pesoKg)
+        {
+            return pesoKg / 0.453;
+        }
+
+        public double GetBollion(double pesoKg)
+        {
+            return pesoKg / 14.59;
+        }
+
+        public double GetPeso (double peso)
+        {
+            Console.WriteLine("1 - Lb");
+            Console.WriteLine("2 - Li");
+            Console.WriteLine("3 - Oz");
+            Console.WriteLine("4 - P");
+            Console.WriteLine("5 - K");
+            Console.WriteLine("6- G");
+            Console.WriteLine("7 - Q");
+
+            Console.WriteLine("Choose one measure.");
+            int measure = Convert.ToInt32(Console.ReadLine());
+
+//            1 Libra = 16 onzas = 453 gramos.
+//1 Lingote = 32,17 libras = 14,59 kg.
+//1 Onza = 0,0625 libras = 28,35 gramos.
+//1 Penique = 0,05 onzas = 1,55 gramos.
+//1 Quintal = 100 libras = 43,3 kg.
+
+
+            switch (measure) {
+                case 1:
+                    return peso/ 0.453;
+                    break;
+                case 2:
+                    return peso / 14.59;
+                    break;
+                case 3:
+                    return peso / 0.00153;
+                    break;
+                case 4:
+                    return peso / 0.02835;
+                    break;
+                case 5:
+                    return peso;
+                    break;
+                case 6:
+                    return peso*1000;
+                    break;
+                case 7:
+                    return peso/43.3;
+                    break;
+            }
+            return peso;
+        }
+    }
+}
---
+++ ObjectPeso/ObjectPeso/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectPeso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Peso p1 = new Peso();
+            Console.WriteLine(p1.GetPeso(10));
+            Console.ReadLine();
+        }
+    }
+}
