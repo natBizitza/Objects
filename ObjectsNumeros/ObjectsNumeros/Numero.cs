670a201fc63027c0c991c209231091fcfca9b498
Natalia Romankevich <40891986+natBizitza@users.noreply.github.com>
2018-10-15 13:55:41 +0200

    Final

---
+++ ObjectsNumeros/ObjectsNumeros/Numero.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectsNumeros
+{
+    class Numero
+    {
+        private int number;
+
+
+        //default constructor
+        public Numero()
+        {
+            number = 0;
+        }
+
+        public Numero(int number)
+        {
+           this.number = number;
+        }
+
+        //Método aniade que permite sumarle un número al valor interno.
+
+        public int SumIt(int number, int newNum)
+        {
+            return number + newNum;
+        }
+        //Método resta que resta un número al valor interno.
+        public int Remainder(int number, int newNum)
+        {
+            return newNum % number;
+        }
+        //Método getValor. Devuelve el valor interno.
+        public int getValor(int number)
+        {
+            return number;
+        }
+        //Método getDoble. Devuelve el doble del valor interno.
+        public int getDouble(int number)
+        {
+            return number * 2;
+        }
+        //Método getTriple. Devuelve el triple del valor interno.
+        public int getTriple(int number)
+        {
+            return number *3;
+        }
+        //Método setNumero. Inicializa de nuevo el valor interno.
+        public int setNumero(int number, int newNum)
+        {
+            return number = newNum;
+        }
+    }
+}
---
+++ ObjectsNumeros/ObjectsNumeros/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectsNumeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Numero n1 = new Numero();
+            Console.WriteLine("The sum is " + n1.SumIt(10,12));
+            Console.WriteLine("The remainder is " + n1.Remainder(10,12));
+            Console.WriteLine("The value is " + n1.getValor(10));
+            Console.WriteLine("The double is " + n1.getDouble(10));
+            Console.WriteLine("The trible is " + n1.getTriple(10));
+            Console.WriteLine("The new value is " + n1.setNumero(10, 100));
+
+
+            Console.ReadLine();
+        }
+    }
+}
