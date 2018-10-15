3f1d9abaa3cec4e8b6b7acefb24592006ddb9b56
Natalia Romankevich <40891986+natBizitza@users.noreply.github.com>
2018-10-15 14:19:39 +0200

    update1

---
+++ objectSatelite/objectSatelite/Satelite.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace objectSatelite
+{
+    public class Satelite
+    {
+        private double meridiano;
+        private double paralelo;
+        private double distancia_tierra;
+        private double desplazamiento;
+
+        public Satelite(double m, double p, double d)
+        {
+            meridiano = m;
+            paralelo = p;
+            distancia_tierra = d;
+        }
+
+        public Satelite()
+        {
+            meridiano = paralelo = distancia_tierra = 0;
+        }
+
+        public void SetPosicion(double m, double p, double d)
+        {
+            this.meridiano = m;
+            this.paralelo = p;
+            this.distancia_tierra = d;
+        }
+
+        public void PrintPosicion()
+        {
+            Console.WriteLine("El satélite se encuentra en el paralelo " + paralelo + " Meridiano "+meridiano+" a una distancia de la tierra de "+distancia_tierra+" Kilómetros");
+            Console.ReadLine();
+        }
+        //Método void VariaAltura(double desplazamiento)
+        public void VariaAltura(double dsp)
+        {
+            this.desplazamiento =  dsp;
+        }
+        //Método boolean EnOrbita()
+        //Método void VariaPosicion(double variap, double variam)
+
+    }
+}
