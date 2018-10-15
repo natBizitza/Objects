05686767d374955eac0e695cb92b6fe3d8cfe229
Natalia Romankevich <40891986+natBizitza@users.noreply.github.com>
2018-10-15 18:14:58 +0200

    Final version (with Iker)

---
+++ ObjectCoche/ObjectCoche/Coche.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectCoche
+{
+    class Coche
+    {
+        private string marca;
+        private string modelo;
+
+        public Coche()
+        {
+
+        }
+
+        public Coche(string marca, string modelo)
+        {
+            this.marca = marca;
+            this.modelo = modelo;
+        }
+        //Implements methods to get and set the value of the attributes, as well as a 
+        //method to display all the information.
+
+        public string GetMarca()
+        {
+            return this.marca;
+        }
+        public void SetMarca(string marca)
+        {
+            this.marca = marca;
+        }
+        public string GetModelo()
+        {
+            return this.modelo;
+        }
+        public void SetModelo(string modelo)
+        {
+            this.modelo = modelo;
+        }
+        public string AllInfo()
+        {
+            return  "Coche con marca " + this.marca + " y modelo " + this.modelo;
+        }
+    }
+}
---
+++ ObjectCoche/ObjectCoche/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectCoche
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Coche c1 = new Coche();
+            c1.SetMarca("ferrari");
+            c1.SetModelo("testarrosa");
+
+            Console.WriteLine(c1.AllInfo());
+            Coche c2 = new Coche("seat", "panda");
+            Console.WriteLine(c2.AllInfo());
+
+            Console.ReadLine();
+        }
+    }
+}
